13edd14bb904d1c4fde8e68a5d5e576103407ef9
Suzy Oborokumo <suzy.oborokumo@cyphercrescent.com>
2025-03-07 15:30:37 +0100

    updated the IFileImporter to include a non-generic interface that the factory can use

---
+++ FileReader/ImportInterface/IFileImporter.cs
@@ -0,0 +1,16 @@
+﻿namespace FileReader.ImportInterface
+{
+    public interface IFileImporter<T> : IFileImporter
+    {
+        new T ImportFile(string filePath);
+
+        object IFileImporter.ImportFile(string filePath) => ImportFile(filePath);
+    }
+
+    public interface IFileImporter
+    {
+        bool CanImport(string filePath);
+        object ImportFile(string filePath);
+    }
+
+}
